40268ff99211289b397f5552abaff40e97ae93af
Davide R. Wiest <davide.wiest2@gmail.com>
2024-04-08 08:37:08 +0200

    aaa

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import numpy as np
+import pandas
 
+genres = pandas.read_csv('in/streaming_genres.csv', dtype=np.int32, delimiter=',', header=0)
+historie = pandas.read_csv('in/streaming_historie.csv', dtype=np.int32, delimiter=',', header=0)
+ratings = pandas.read_csv('in/streaming_ratings.csv', dtype=np.int32, delimiter=',', header=0)
 
-
+def get_most_similar(movies_genres, available_movies):
+    movies_avg = np.mean(movies_genres, axis=0)
+    most_similar_movie = available_movies.sort
